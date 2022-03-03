c3163e6b92c554f06e73e14aa7d27b0ee51242ab
yfcmrbyc <78963995+yfcmrbyc@users.noreply.github.com>
2022-03-03 16:32:41 +0300

    Update task-list.js

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import Task from '../task/task';
 import './task-list.css';
 
-export default class TaskList extends Component {
-  static propTypes = {
-    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
-    onToggleDone: PropTypes.func.isRequired,
-    onDeleted: PropTypes.func.isRequired,
-  };
+function TaskList({todos, onDeleted, onToggleDone}) {
 
   renderTask = (todos) =>
     todos.map((item) => {
       const { isHidden, ...itemProps } = item;
-      const { onDeleted, onToggleDone } = this.props;
 
       const liClass = classNames({
         hidden: isHidden,
@@ -23,13 +17,18 @@ export default class TaskList extends Component {
 
       return (
         <li key={item.id} className={liClass}>
-          <Task {...itemProps} onDeleted={() => onDeleted(item.id)} onToggleDone={() => onToggleDone(item.id)} />
+          <Task {...itemProps} onDeleted={onDeleted} onToggleDone={onToggleDone} />
         </li>
       );
     });
 
-  render() {
-    const { todos } = this.props;
-    return <ul className="task-list">{this.renderTask(todos)}</ul>;
-  }
+   return <ul className="task-list">{this.renderTask(todos)}</ul>;
 }
+
+TaskList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggleDone: PropTypes.func.isRequired,
+    onDeleted: PropTypes.func.isRequired,
+  };
+
+export default TaskList;
